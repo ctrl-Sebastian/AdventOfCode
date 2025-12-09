ad6b8edce51a9879bddcf8499d709eb327bce9b0
Sebastian De Leon <sebastianipl0131@gmail.com>
2025-12-09 08:28:43 -0500

    start day 4 2025

---
+++ 2025/4/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+        ifstream input_file;
+        input_file.open("input.txt");
+
+        int total_rolls = 0;
+        vector<vector<char>> map;
+
+
+        cout << "Total rolls of paper: " << total_rolls << endl;
+        input_file.close();
+        return 0;
+}
